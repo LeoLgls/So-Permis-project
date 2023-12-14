17b26a988bb30cb293c67c223830cba9ff82e199
Divini <92796539+Diviini@users.noreply.github.com>
2023-12-14 10:18:22 +0100

    Insertion du GlobalStyle.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Accueil from './views/Accueil/Accueil'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import GlobalStyle from "./utils/style/GlobalStyle.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle/>
       <Routes>
         <Route path="/" element={<Accueil />}></Route>
       </Routes>
---
+++ frontend/src/utils/style/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const StyledGlobalStyle = createGlobalStyle`
+    * {
+        font-family: "montserrat",sans-serif;
+    }
+`
+
+function GlobalStyle() {
+    return <StyledGlobalStyle/>
+}
+
+export default GlobalStyle
