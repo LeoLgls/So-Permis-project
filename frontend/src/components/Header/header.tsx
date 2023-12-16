2e23eabb846a7ae973044a0bc66c449572c6e795
Divini <92796539+Diviini@users.noreply.github.com>
2023-12-16 17:25:25 +0100

    Suppression d'imports

--- frontend/src/components/Header/header.tsx
+++ frontend/src/components/Header/header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import { StyledLink } from '../../utils/style/Atoms';
