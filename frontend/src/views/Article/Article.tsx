4490e8705874c56166168e3e41372c505387a3b0
Divini <92796539+Diviini@users.noreply.github.com>
2023-12-19 11:14:01 +0100

    Fin de la page Article.tsx

--- frontend/src/views/Article/Article.tsx
+++ frontend/src/views/Article/Article.tsx
@@ -4,53 +4,72 @@ import {Article} from "../../utils/models/models.tsx";
 import {articleList} from "../../services/service.tsx";
 import imgTest from '../../assets/img/imgTest3.jpg'
 import colors from "../../utils/style/colors.tsx";
+import {MainContainer, TitreSection} from "../../utils/style/elements.tsx";
 
 const ArticleContainer = styled.div`
-  height: 100vh;
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background-color: ${colors.backgroundBlanc};
 `;
 
 const HeroArticle = styled.div`
-    height: 5rem;
+    height: 25rem;
     pointer-events: none;
     position: relative;
     width: 100%;
+    margin-bottom: 2rem;
     
 `
 
 const TransitionArticle = styled.div`
-    background: linear-gradient("0deg", ${colors.backgroundBlanc} 0%, ${colors.backgroundBlanc} 100%);;
-    height: 5rem;
+    background: linear-gradient(0, rgba(241,241,241,1) 0%, rgba(241,241,241,0.66) 50%, rgba(241,241,241,0) 100%);    
+    height: 25rem;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
 
 `
 
 const ImgArticle = styled.img`
     top: 0;
     left: 0;
-    position: absolute;
+    object-fit: cover;
+    width: 100%;
+    height: 25rem;
 `
 
 const ArticleContent = styled.div`
-  
+  display: flex;
+  flex-direction: column;
 `;
 
 const ArticleTitle = styled.h1`
+    position: absolute;
+    bottom: 0;
+    padding-left: 15vw;
+    padding-right: 15vw;
 
+    @media (max-width: 1090px) {
+        padding-left: 5vw;
+        padding-right: 5vw;
+    }
 `;
 
 const ArticleMeta = styled.div`
-
+    height: 1rem;
 `;
 
 const ArticleBody = styled.div`
-  
+    min-height: 50vh;
 `;
 
-function ArticlePage() {
+const PublicationSpan = styled.span`
+    color: ${colors.vert};
+`
 
+const PContenu = styled.p`
+    
+`
+
+function ArticlePage() {
 
   const { articleNumberParam } = useParams();
   const articleNumberAsNumber: number     = Number(articleNumberParam);
@@ -82,12 +101,19 @@ function ArticlePage() {
             <TransitionArticle />
             <ArticleTitle>{data.titre}</ArticleTitle>
           </HeroArticle>
-          <ArticleMeta>
-            <span>publier le : {`${data.date.toLocaleDateString()}`}</span>
-          </ArticleMeta>
-          <ArticleBody>
-            <span>{data.contenu}</span>
-          </ArticleBody>
+
+          <MainContainer theme={'blanc'}>
+            <ArticleMeta>
+              <PublicationSpan>Publication le : {`${data.date.toLocaleDateString()}`}</PublicationSpan>
+            </ArticleMeta>
+            <ArticleBody>
+              <TitreSection theme={'blanc'}>Contenu</TitreSection>
+              <PContenu>{data.contenu}</PContenu>
+              <TitreSection theme={'blanc'}>Source</TitreSection>
+              <PContenu>{data.source}</PContenu>
+            </ArticleBody>
+          </MainContainer>
+
         </ArticleContent>
       //)
       }
