55b5f514691d9a6d4b63b2834761fe2bba0c0a2d
Gamecraft400 <102668085+Gamecraft400@users.noreply.github.com>
2023-12-19 23:17:24 +0100

    ajout gestion de l'heure automatique

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -21,6 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/api/contact/sendEmail','ContactsController.sendEmail');
+Route.get('/api/contact/opening-hours', 'ContactsController.getOpeningHours');
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
